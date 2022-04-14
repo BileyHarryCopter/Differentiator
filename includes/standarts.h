3cfcd2b7f7d2078e269ddbf6749ceff2978f3025
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-04-15 02:13:04 +0300

    directory with main include files

---
+++ includes/standarts.h
@@ -0,0 +1,12 @@
+#ifndef STANDARTS
+#define STANDARTS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <assert.h>
+#include <ctype.h>
+#include <math.h>
+
+#endif
