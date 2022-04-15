5c6214d1885c2c916c8f7c128b8565ce04d630b6
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-04-15 21:54:28 +0300

    main

--- main.c
+++ main.c
@@ -1,13 +1,21 @@
-#include "lexer/lexer.h"
 #include "includes/standarts.h"
+#include "includes/constant.h"
+#include "lexer/lexer.h"
+#include "parcer/parcer.h"
 
 int main (void)
 {
+    node_t *tree = NULL;
     lexem_array_t *lexus = LexsInit (CAP_INIT);
-    if (LexsFill (lexus, "input.txt") == NO_ERROR)
+
+    if (LexsFill (lexus, "logs/input.txt") == NO_ERROR)
     {
-        LexsPrint (lexus, "output.txt");
+        LexsPrint (lexus, "logs/output.txt");
+        tree = Build_Syntax_Tree (lexus);
+        TreePrint (tree, "logs/outtree.txt");
     }
+
+    TreeDelete (tree);
     LexsDelete (lexus);
     return 0;
 }
